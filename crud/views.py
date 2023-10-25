1353035bf85de9811bda289ec0589db23d5c9c45
AibekYrysbekov <yrysbekovayba@gmail.com>
2023-10-26 02:40:34 +0600

    creat app 'crud', add MTV funcions

--- crud/views.py
+++ crud/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import ToDo
 
-# Create your views here.
+
+def index(request):
+    todos = ToDo.objects.all()
+    return render(request, 'config/index.html', {'todo_list': todos, 'title': 'Главная страница'})
