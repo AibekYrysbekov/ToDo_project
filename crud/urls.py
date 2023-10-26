4d2a1b233854748d27d287ffab56a87426f7a5f7
AibekYrysbekov <yrysbekovayba@gmail.com>
2023-10-27 01:55:44 +0600

    adding CRUD functionality

--- crud/urls.py
+++ crud/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('create', views.create, name='create'),
+    path('update/<int:todo_id>', views.update, name='update'),
+    path('delete/<int:todo_id>', views.delete, name='delete'),
+    path('edit/<int:todo_id>/', views.edit_todo, name='edit'),
+
 ]
--- crud/views.py
+++ crud/views.py
@@ -1,7 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods
 from .models import ToDo
 
 
 def index(request):
     todos = ToDo.objects.all()
     return render(request, 'config/index.html', {'todo_list': todos, 'title': 'Главная страница'})
+
+
+@require_http_methods(['POST'])
+def create(request):
+    title = request.POST['title']
+    description = request.POST['description']
+    todo = ToDo(title=title, description=description)
+    todo.save()
+    return redirect('index')
+
+
+def update(request, todo_id):
+    todo = ToDo.objects.get(id=todo_id)
+    todo.is_complete = not todo.is_complete
+    todo.save()
+
+    context = {
+        'title': todo.title,
+        'description': todo.description,
+    }
+    return redirect('index')
+
+
+def delete(request, todo_id):
+    todo = ToDo.objects.get(id=todo_id)
+    todo.delete()
+    return redirect('index')
+
+
+def edit_todo(request, todo_id):
+    todo = ToDo.objects.get(id=todo_id)
+
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        todo.title = title
+        todo.description = description
+        todo.save()
+        return redirect('index')
+
+    context = {
+        'todo': todo,
+    }
+    return render(request, 'edit_todo.html', context)
